eefd2eabb55ceccde935a33c2ae4c5bd0e971e8c
Gustavo Barragan <dev@droni.co>
2024-11-25 21:41:37 -0500

    header

---
+++ components/Ui/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <div class="container py-2 mx-auto flex">
+      <NuxtLink to="/" class="md:flex" title="Kosante | La mejor herramienta para amantes de la nutrición.">
+        <img src="~/assets/img/kotheme/logo.svg" alt="Kosante | La mejor herramienta para amantes de la nutrición." class="h-12" />
+      </NuxtLink>
+      <nav class="md:grow md:flex justify-end">
+        <NuxtLink to="/" class="p-2">Blog de nutrición</NuxtLink>
+        <NuxtLink to="/about" class="p-2">Alimentos DB</NuxtLink>
+        <NuxtLink to="/about" class="p-2">Herramientas</NuxtLink>
+        <NuxtLink to="/about" class="p-2">Foro</NuxtLink>
+        <NuxtLink to="/contact" class="p-2">Consulta Nutricional</NuxtLink>
+        <NuxtLink to="/contact" class="p-2">Mi cuenta</NuxtLink>
+      </nav>
+    </div>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
   <NuxtRouteAnnouncer />
+  <UiHeader />
   <slot />
 </template>
